03d390dff8a82394152c06d802d7e5843f0046da
yuriassuncx <killerkoll2012@gmail.com>
2025-07-10 17:58:58 -0300

    refactor: isSkippable and cleanResponse functions to improve logic

--- translate/utils.ts
+++ translate/utils.ts
@@ -2,6 +2,7 @@ export const makeCacheKey = (val: unknown): string => JSON.stringify(val);
 
 export const isSkippable = (val: unknown): boolean => {
   if (!val) return true;
+  if (typeof val === 'number') return true;
   if (typeof val === 'string') {
     return val.startsWith('gid://') || val.startsWith('shopify://') || val.includes('cdn.shopify.com');
   }
@@ -19,9 +20,6 @@ export const serializeCSVField = (val: unknown): string => {
 
 export const cleanResponse = (input: unknown, response: string): unknown => {
   const trimmed = response.trim();
-  const containsComments = /mantive|tradu[çc][aã]o|explica[çc][aã]o|não existe necessidade/i.test(trimmed);
-
-  if (containsComments) return input;
 
   if (typeof input === 'string' && /^".*"$/.test(trimmed)) {
     return trimmed.slice(1, -1);
